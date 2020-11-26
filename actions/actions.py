5f1189ebbee706ab31c031d647bc237f3d7292bf
sauravjoshi23 <sauravjoshi2362000@gmail.com>
2020-11-26 23:58:48 +0530

    added general tips about covid and collected user information - part5

--- actions/actions.py
+++ actions/actions.py
@@ -1,27 +1,21 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
-#
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/custom-actions
+from typing import Any, Text, Dict, List
+import requests
+import json
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.events import SlotSet,UserUtteranceReverted
 
 
-# This is a simple example for a custom action which utters "Hello World!"
+class NameAction(Action):
+    def name(self) -> Text:
+        return "action_your_name"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+            slot_name=str(tracker.get_slot("NAME"))
+
+            message = "Hi {}  Do you want more about Corna Virus in Your Place!!example:--im leaving in statename and my district is !".format(slot_name)
+
+            dispatcher.utter_message(text=message)
+            return []
